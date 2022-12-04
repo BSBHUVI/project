26911c2cf9500032d86ff71a4aa0c3966cc7b179
bhuvan <bhuvansbhuvan467@gmail.com>
2022-12-04 12:30:04 +0530

    d

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance=axios.create({
-    baseURL:"http://localhost:5000/"
-    // baseURL:"https://project-backend-ssce.onrender.com"
+    // baseURL:"http://localhost:5000/"
+    baseURL:"https://project-backend-ssce.onrender.com"
 })
 export default instance
