3034c0f0d1f0ef215db75924d49a68cdb3d9e142
bhuvan <bhuvansbhuvan467@gmail.com>
2023-01-26 18:58:34 +0530

    Ss

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance=axios.create({
-  baseURL:"http://localhost:5000"
-  // baseURL:"https://wandering-cyan-pants.cyclic.app"
+  // baseURL:"http://localhost:5000"
+  baseURL:"https://wandering-cyan-pants.cyclic.app"
 
 })
 export default instance
